bef159f3b218b858d8c9656180a693fdf33bcbaf
Jeremy Gao <wensimehrp@gmail.com>
2025-06-04 11:27:29 -0700

    feat: add some kde packages

--- modules/packages.nix
+++ modules/packages.nix
@@ -19,7 +19,7 @@ let
 in
 {
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  # nixpkgs.config.allowUnfree = true;
 
   # Firefox
   programs.firefox.enable = true;
@@ -64,6 +64,20 @@ in
 
     megacmd
     hplip
+
+    # kde stuff
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    kdiff3 # Compares and merges 2 or 3 files or directories
+    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
+    hardinfo2 # System information and benchmarks for Linux systems
+    wayland-utils # Wayland utilities
+    wl-clipboard # Command-line copy/paste utilities for Wayland
   ];
 
   # Steam
