c1a94e850b7a14ba71f260b33a33f4b3801424c0
Jeremy Gao <wensimehrp@gmail.com>
2025-06-04 11:28:04 -0700

    feat: flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+
+        modules = [
+          ./configuration.nix
+          nixos-hardware.nixosModules.lenovo-legion-16irx8h
+        ];
+      };
+    };
+}
