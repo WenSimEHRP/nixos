b06108a92792c86d00f3d2d2166e03d34070b96f
Jeremy Gao <wensimehrp@gmail.com>
2025-08-04 23:57:30 +0800

    feat: v4l2loopback for obs virtual camera

--- modules/system.nix
+++ modules/system.nix
@@ -16,6 +16,15 @@
   i18n.defaultLocale = "en_CA.UTF-8";
   services.automatic-timezoned.enable = true;
 
+  # for obs
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
   # networking
   services.mihomo = {
     enable = true;
